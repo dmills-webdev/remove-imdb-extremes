9032de0a0c77823d827676520e3a4796048f45d9
dmills-webdev <gasmazk73@gmail.com>
2024-02-09 21:28:45 +0000

    Add cors whitelist and allow origin/headers

--- process.js
+++ process.js
@@ -39,6 +39,18 @@ const MediaItem = mongoose.model(
 /* ------------ Init App ----------- */
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  const corsWhitelist = [
+      'https://www.imdb.com/',
+      'https://imdb.com/',
+  ];
+  if ( corsWhitelist.includes(req.headers.origin) ) {
+      res.header('Access-Control-Allow-Origin', req.headers.origin);
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  }
+
+  next();
+});
 
 /* ------------ Endpoints ---------- */
 
